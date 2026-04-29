0d7659064d4f84a624703fe986eab6fc4210b303
NewStart-c <2161774302@qq.com>
2026-04-29 15:15:31 +0800

    1、教练学员签到

---
+++ src/views/coach/sign/index.vue
@@ -0,0 +1,18 @@
+<template>
+  <el-table :data="list">
+    <el-table-column label="学员" prop="memberId" />
+    <el-table-column label="课程" prop="courseId" />
+    <el-table-column label="操作">
+      <template #default="scope">
+        <el-button @click="sign(scope.row.reservationId)">签到</el-button>
+      </template>
+    </el-table-column>
+  </el-table>
+</template>
+
+<script setup>
+import { coachReservation, coachSign } from '@/api/coach'
+const list = ref([])
+onMounted(() => coachReservation().then(res => list.value = res.rows))
+const sign = (id) => coachSign(id).then(() => alert('签到成功'))
+</script>
