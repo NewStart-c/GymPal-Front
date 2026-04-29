fcc3b813c61d80388feff9824cdd52f7328680d8
NewStart-c <2161774302@qq.com>
2026-04-29 15:14:19 +0800

    1、教练个人资料

---
+++ src/views/coach/trainer/index.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    <el-form v-model="form">
+      <el-form-item label="专长"><el-input v-model="form.specialties" /></el-form-item>
+      <el-form-item label="经验"><el-input v-model="form.experience" type="textarea" /></el-form-item>
+      <el-button @click="submit">保存</el-button>
+    </el-form>
+  </div>
+</template>
+
+<script setup>
+import { coachProfile, updateCoachProfile } from '@/api/coach'
+const form = ref({})
+onMounted(() => coachProfile().then(res => form.value = res.data))
+const submit = () => updateCoachProfile(form.value).then(() => alert('保存成功'))
+</script>
