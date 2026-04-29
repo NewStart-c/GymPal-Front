30bf120fb01ef745efe050c00c84ec6a97f68e47
NewStart-c <2161774302@qq.com>
2026-04-29 15:13:44 +0800

    1、教练工作台搭建

---
+++ src/api/coach/index.js
@@ -0,0 +1,39 @@
+import request from '@/utils/request'
+
+// 工作台统计
+export function coachDashboard() {
+    return request({ url: '/coach/dashboard', method: 'get' })
+}
+
+// 个人资料
+export function coachProfile() {
+    return request({ url: '/coach/profile', method: 'get' })
+}
+export function updateCoachProfile(data) {
+    return request({ url: '/coach/profile', method: 'put', data })
+}
+
+// 我的排班
+export function coachSchedule(query) {
+    return request({ url: '/coach/schedule', method: 'get', params: query })
+}
+
+// 我的课程
+export function coachCourse(query) {
+    return request({ url: '/coach/course', method: 'get', params: query })
+}
+
+// 我的预约
+export function coachReservation(query) {
+    return request({ url: '/coach/reservation', method: 'get', params: query })
+}
+
+// 学员签到
+export function coachSign(resId) {
+    return request({ url: '/coach/sign/' + resId, method: 'put' })
+}
+
+// 我的评价
+export function coachEvaluation(query) {
+    return request({ url: '/coach/evaluation', method: 'get', params: query })
+}
---
+++ src/views/coach/dashboard/index.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class="dashboard">
+    <div class="box"><label>今日课程</label><span>{{ data.courseCount }}</span></div>
+    <div class="box"><label>学员预约</label><span>{{ data.reservationCount }}</span></div>
+    <div class="box"><label>评分</label><span>{{ data.scoreAvg }}</span></div>
+  </div>
+</template>
+
+<script setup>
+import { coachDashboard } from '@/api/coach'
+const data = ref({})
+onMounted(() => coachDashboard().then(res => data.value = res.data))
+</script>
