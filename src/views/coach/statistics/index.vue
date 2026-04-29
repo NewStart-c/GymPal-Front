0cdfb442b24330b7367dc4231bab5e2029873575
NewStart-c <2161774302@qq.com>
2026-04-29 15:16:21 +0800

    1、教练业绩统计

---
+++ src/views/coach/statistics/index.vue
@@ -0,0 +1,12 @@
+<template>
+  <div>
+    <h3>本月上课：{{ courseCount }} 节</h3>
+    <h3>评分：{{ scoreAvg }}</h3>
+  </div>
+</template>
+
+<script setup>
+import { coachDashboard } from '@/api/coach'
+const data = ref({})
+onMounted(() => coachDashboard().then(res => data.value = res.data))
+</script>
